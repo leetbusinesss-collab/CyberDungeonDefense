de9971af0b896eb937b113ae1e860720c36eb922
leetbusinesss-collab <leet.businesss@gmail.com>
2026-09-04 03:37:16 +0300

    add pygame main

---
+++ src/main_pygame.py
@@ -0,0 +1,4 @@
+import pygame, sys
+from src.config import *
+pygame.init(); screen=pygame.display.set_mode((1024,768))
+print("Pygame UI initialized. Run buildozer android debug deploy run to build APK.")
